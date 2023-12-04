afd37ac627cb29dafea446d515e69b346d2f07a8
jipenaflor <rome.img23@gmail.com>
2023-12-04 14:41:47 +0800

    Added exercises 3.12-3.18

--- part3/index.js
+++ part3/index.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const app = express()
+
 const morgan = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
-const app = express()
+// using mongodb
+require('dotenv').config()
+const Person = require('./models/person')
 
 // middleware functions used before routes since we want them to be executed
 // before the route handlers are called
 app.use(express.json())
 app.use(cors())
 
+/* //hardcoded objects
 let persons = [
     { 
         "id": 1,
@@ -31,38 +37,7 @@ let persons = [
         "number": "39-23-6423122"
     }
 ]
-
-// fetch persons
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
-
-// route info
-app.get('/info', (request, response) => {
-    const date = new Date()
-    response.send(`<p>Phonebook has info for ${persons.length} people</p>
-        <p>${date}</p>`)
-})
-
-// fetch single person
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-})
-
-// delete a person
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
-})
+*/
 
 // middleware functions used before routes since we want them to be executed
 // before the route handlers are called
@@ -81,10 +56,60 @@ morgan.token('request-body', (req, res) => {
     return JSON.stringify(req.body)
 })
 
-// add a person
+// fetch persons
+app.get('/api/persons', (request, response) => {
+  //response.json(persons)
+  Person.find({}).then(person => {
+    response.json(person)
+  })
+})
+
+// route info
+app.get('/info', (request, response) => {
+    const date = new Date()
+    Person.find({}).then(person => {
+        response.send(`<p>Phonebook has info for ${person.length} people</p>
+        <p>${date}</p>`)
+    })
+})
+
+// fetch single person
+app.get('/api/persons/:id', (request, response, next) => {
+    /*const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    */
+    Person.findById(request.params.id).then(person => {
+        if (person) {
+            response.json(person)
+        } else {    // not matching object
+            response.status(404).end()
+        }
+    }).catch(error => {     // rejected promise
+        next(error)
+    })
+})
+
+// delete a person
+app.delete('/api/persons/:id', (request, response, next) => {
+    /*
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+    */
+   Person.findByIdAndDelete(request.params.id).then(result => {
+    response.status(204).end()
+   }).catch(error => next(error))
+})
+
+// helper function
+/*
 const generateId = () => {
     return Math.floor(Math.random() * 1000)
 }
+*/
+
+// add a person
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
@@ -94,30 +119,73 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
+    /*
     if (persons.find(person => person.name === body.name)) {
         return response.status(400).json({
             error: 'the name already exists in the phonebook'
+        })
+    }
+    */
+
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+        //id: generateId()
+    })
+
+    /*
+    persons = persons.concat(person)
+    response.json(person)
+    */
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+})
+
+// update person
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    if (!body.name && !body.number) {
+        return response.status(400).json({
+            error: 'the name or number is missing'
         })
     }
 
     const person = {
         name: body.name,
         number: body.number,
-        id: generateId()
     }
-    
-    persons = persons.concat(person)
-    response.json(person)
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 // using middlewares after route if no route handles the http request
-/*
+// placed last because any request would return 404 if this is placed first
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
+}
+
 app.use(unknownEndpoint)
-*/
+
+// using middleware to handle error
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+    
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id'})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
---
+++ part3/mongo.js
@@ -0,0 +1,51 @@
+// mongoose is a higher level api than mongodb driver library
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+} 
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://jipenaflor:${password}@cluster0.twnad0r.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+// add contact
+const addPerson = (name, number) => {
+    const person = new Person({
+        name: name,
+        number: number,
+    })
+
+    person.save().then(result => {
+        console.log(`Added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+// fetch contacts
+const fetchPersons = () => {
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
+
+if (process.argv.length == 5) {
+    addPerson(process.argv[3], process.argv[4])
+} else if (process.argv.length == 3) {
+    fetchPersons()
+}
