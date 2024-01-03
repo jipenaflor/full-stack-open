487d1d99b85353ad2487cde36894200a71c28daa
jipenaflor <rome.img23@gmail.com>
2024-01-03 20:46:22 +0800

    Implemented bloglist backend with postgresql

---
+++ part13/cli.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const { Sequelize, QueryTypes } = require('sequelize')
+
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+const main = async () => {
+  try {
+    const blogs = await sequelize.query('SELECT * FROM blogs', { type: QueryTypes.SELECT })
+    blogs.map(blog => console.log(`${ blog.author }: ${ blog.title }, ${ blog.likes } likes`))
+    sequelize.close()
+  } catch (error) {
+    console.log('Unable to connect to the database: ', error)
+  }
+}
+
+main()
