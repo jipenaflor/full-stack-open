9ae732c30e96bf6344e239ac59747b5e205ef98e
jipenaflor <rome.img23@gmail.com>
2024-01-03 22:22:14 +0800

    Arranged app structure

---
+++ part13/controllers/blogs.js
@@ -0,0 +1,37 @@
+const blogsRouter = require('express').Router()
+const { Blog } = require('../models')
+
+blogsRouter.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+  
+blogsRouter.post('/', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+  
+blogsRouter.get('/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if (blog) {
+    res.json(blog)
+  } else {
+    req.status(404).end()
+  }
+})
+  
+blogsRouter.delete('/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  await Blog.destroy({
+    where: {
+      id: blog.id
+    } 
+  })
+  return res.status(204).end()
+})
+
+module.exports = blogsRouter
